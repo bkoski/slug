776ee241a700ac858a8aa45130fa682f7a883991
Ben Koski <bkoski@nytimes.com>
2009-09-01 12:06:33 -0400

    moved test models to test/models so that they are loaded once per test run

---
+++ test/models.rb
@@ -0,0 +1,23 @@
+# Used to test slug behavior in general
+class Article < ActiveRecord::Base
+  slug :headline  
+end
+
+# Used to test alternate slug column
+class Person < ActiveRecord::Base
+  slug :name, :column => :web_slug
+end
+
+# Used to test invalid method names
+class Company < ActiveRecord::Base
+  slug :name
+end
+
+# Used to test slugs based on methods rather than database attributes
+class Event < ActiveRecord::Base
+  slug :title_for_slug
+  
+  def title_for_slug
+    "#{title}-#{location}"
+  end
+end
