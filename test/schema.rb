134aecd2a0848c52c6460951f81ca8359a6aede0
Ben Koski <bkoski@nytimes.com>
2009-09-01 11:51:31 -0400

    added events model and descriptions of model use

--- test/schema.rb
+++ test/schema.rb
@@ -2,12 +2,24 @@ ActiveRecord::Schema.define(:version => 1) do
 
   create_table "articles", :force => true do |t|
     t.column "headline", "string"
+    t.column "section", "string"
     t.column "slug", "string"
   end
   
   create_table "people", :force => true do |t|
     t.column "name", "string"
     t.column "web_slug", "string"
+  end
+  
+  create_table "companies", :force => true do |t|
+    t.column "name", "string"
+    t.column "slug", "string"
+  end
+  
+  create_table "events", :force => true do |t|
+    t.column "title", "string"
+    t.column "location", "string"
+    t.column "slug", "string"
   end
   
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -23,10 +23,26 @@ silence_stream(STDOUT) do
   load(File.dirname(__FILE__) + "/schema.rb")
 end
 
+# Used to test slug behavior in general
 class Article < ActiveRecord::Base
-  slug :headline
+  slug :headline  
 end
 
+# Used to test alternate slug column
 class Person < ActiveRecord::Base
   slug :name, :column => :web_slug
+end
+
+# Used to test invalid method names
+class Company < ActiveRecord::Base
+  slug :name
+end
+
+# Used to test slugs based on methods rather than database attributes
+class Event < ActiveRecord::Base
+  slug :title_for_slug
+  
+  def title_for_slug
+    "#{title}-#{location}"
+  end
 end
