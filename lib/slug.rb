80bf5ee8e40a65da0cc70fcb4ecb00dc48ff7886
Ben Koski <bkoski@nytimes.com>
2011-03-16 16:48:28 -0400

    added rails version check

--- lib/slug.rb
+++ lib/slug.rb
@@ -4,3 +4,7 @@ require File.join(File.dirname(__FILE__), 'slug', 'ascii_approximations')
 if defined?(ActiveRecord)
   ActiveRecord::Base.instance_eval { extend Slug::ClassMethods }
 end
+
+if defined?(Rails) && Rails.version.to_i < 3
+  raise "This version of slug requires Rails 3 or higher"
+end
